9ced053cc1e14da8aca5645ae44d716e63cf7ebe
Chuckie Granada <devchuckcamp@gmail.com>
2017-12-10 14:37:38 +0800

    redux initialized

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+
+import {createStore, applyMiddleware } from	"redux";
+import { Provider }	from "react-redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import registerServiceWorker from './registerServiceWorker';
+
+//Reducer
+import rootReducer from "./rootReducer";
+
+//CSS
+import "semantic-ui-css/semantic.min.css";
+
+const store = createStore(
+	rootReducer,
+	composeWithDevTools(applyMiddleware(thunk))
+);
+
+
+ReactDOM.render(
+	<BrowserRouter>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</BrowserRouter>,
+	document.getElementById("root")
+);
+registerServiceWorker();
