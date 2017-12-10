d762ea1f349ac1a5d1fb70dd0724445ae374b609
Chuckie Granada <devchuckcamp@gmail.com>
2017-12-10 16:09:10 +0800

    API Service

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import LoginForm from "../forms/LoginForm";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { login } from "../../actions/auth";
 
 
 class LoginPage extends React.Component	{
-	submit = (data)	=>{
-		console.log(data,'data');
-	}
+	// submit = (data)	=> {
+	// 	console.log(data,'data');
+	// }
+
+	submit = (data)	=> this.props.login(data).then(()	=>	this.props.history.push("/"));
 
 	render(){
 		return	(
@@ -18,4 +23,11 @@ class LoginPage extends React.Component	{
 	}
 }
 
-export default LoginPage;
+LoginPage.propTypes =	{
+	history: PropTypes.shape({
+		push: PropTypes.func.isRequired
+	}).isRequired,
+	login: PropTypes.func.isRequired
+}
+
+export default connect(null,{ login })(LoginPage);
